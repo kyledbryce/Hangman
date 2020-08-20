01e354059f7ffc8aa795f454ea3643179b39af74
kyledbryce <69512352+kyledbryce@users.noreply.github.com>
2020-08-20 23:46:50 +0100

    Hangman Final
    1st file was an extensive list of conditions and loops. 2nd file, I put in a few functions. This 3rd and final file, I cleared up some variable nomenclature to make it read better.

--- my hangman.py
+++ my hangman.py
@@ -1,96 +1,107 @@
 def not_in_answer():
-    global already_chosen
-    already_chosen += guess
-    fifty_lines()
+    global characters_already_used
+    characters_already_used += latest_guess
+    clear_screen()
     global guesses_remaining
     guesses_remaining -= 1
-    print(hang_stage[6 - guesses_remaining] + "\n\n\n")
+    show_current_hang_stage()
 
 
-def fifty_lines():
+def clear_screen():
     for x in range(50):
         print()
 
 
-def lives():
+def show_lives_remaining():
     if guesses_remaining == 1:
         print("You have " + str(guesses_remaining) + " life left.")
     else:
         print("You have " + str(guesses_remaining) + " lives left.")
 
 
-def wrong_guess():
-    fifty_lines()
-    print(hang_stage[6])
-    print(text[4])
-    global game_over
-    game_over = True
+def wrong_guess_ends_game():
+    clear_screen()
+    print(illustration_of_hanged_stage[6])
+    print("Your guess was incorrect. The answer was " + case_sensitive_answer + ".")
+    global game_concluded
+    game_concluded = True
 
 
-def while_not_choice():
-    global choice, game_over, answer_limit, ON
+def ask_user_if_they_want_to_play_again():
+    global choice, game_concluded, player_one_answer_not_yet_chosen, ON
     while not choice:
-        play_again = input(text[3]).lower()
+        play_again = input("Would you like to play again? (yes/no): ").lower()
         if play_again == "yes":
             choice = True
-            game_over = False
-            answer_limit = False
+            game_concluded = False
+            player_one_answer_not_yet_chosen = False
         elif play_again == "no":
-            fifty_lines()
-            print(text[1])
+            clear_screen()
+            print("\n\n\nPROGRAM TERMINATED!!!\n\n\n")
             choice = True
             ON = False
         else:
-            fifty_lines()
-            print(text[2])
+            clear_screen()
+            print("Please choose only Yes or No.\n")
 
 
-def invalid():
-    fifty_lines()
-    print(hang_stage[6 - guesses_remaining] + "\n\n\n")
+def two_space_bar_invalid_input():
+    clear_screen()
+    show_current_hang_stage()
     print("Invalid choice. You typed at least 2 spaces consecutively.")
-    lives()
+    show_lives_remaining()
+
+
+def show_current_hang_stage():
+    print(illustration_of_hanged_stage[6 - guesses_remaining] + "\n\n\n")
 
 
 ON = True
+
 while ON:
-    fifty_lines()
+    clear_screen()
     print("TWO PLAYER HANGMAN\n")
-    yet_to_be_chosen = "abcdefghijklmnopqrstuvwxyz1234567890!?£$%&@#()<>/*-+=^.,;:\"'"
-    already_chosen = ""
-    answer_limit = False
-    answer_limit_count = 0
-    raw_answer = ""
-    answer = ""
-    while not answer_limit:
-        raw_answer = input("Player 1, please choose a secret word, phrase or letter for Player 2 to guess: ")
-        answer = raw_answer.lower()
-        for i in range(len(answer)):
-            if answer[i] not in yet_to_be_chosen and answer[i] != " ":
-                answer_limit_count += 1
-        if answer_limit_count == 0:
-            answer_limit = True
+    characters_not_used_yet = "abcdefghijklmnopqrstuvwxyz1234567890"
+    characters_already_used = ""
+    case_sensitive_answer = ""
+    lower_case_answer = ""
+    player_one_answer_not_yet_chosen = True
+    while player_one_answer_not_yet_chosen:
+        case_sensitive_answer = input("Player 1, please choose a : word, phrase, letter, number, equation or ANYTHING for Player 2 to guess: ")
+        lower_case_answer = case_sensitive_answer.lower()
+        if len(case_sensitive_answer) == 0:
+            clear_screen()
+            print("\nYou just hit the return key. Please only choose characters from " + characters_not_used_yet + "\n")
+        elif case_sensitive_answer[0] == " ":
+            clear_screen()
+            print("\nYour chosen answer begins with a space. This is forbidden. Please only choose characters from " + characters_not_used_yet + "\n")
         else:
-            print("\nPlease only choose characters from " + yet_to_be_chosen + "\n")
-            answer_limit_count = 0
-    fifty_lines()
+            for i in range(len(lower_case_answer)):
+                if lower_case_answer[i] not in characters_not_used_yet and lower_case_answer[i] != " ":
+                    clear_screen()
+                    print("\nPlease only choose characters from " + characters_not_used_yet + "\n")
+                else:
+                    player_one_answer_not_yet_chosen = False
+    clear_screen()
     print("You have 6 attempts to guess the correct characters, and you can only")
-    print("guess the word(s) once. Here, a word is defined as 2 letters or more.\n")
-    guess_list = []
-    for i in range(len(answer)):
-        if answer[i] == " ":
-            guess_list.append(" ")
+    print("guess the whole answer once. Here, a guess at the whole answer is defined as 2 valid characters or more.\n")
+    array_of_revealed_concealed_characters = []
+    allowed_special_characters = "!?£$%&@#()<>/*-+=^.,;:\"'"
+    for i in range(len(lower_case_answer)):
+        if lower_case_answer[i] == " ":
+            array_of_revealed_concealed_characters.append(" ")
+        elif lower_case_answer[i] in allowed_special_characters:
+            array_of_revealed_concealed_characters.append(lower_case_answer[i])
         else:
-            guess_list.append("_")
-    guess_update = ""
-    for i in range(len(guess_list)):
-        guess_update += guess_list[i]
-    guess = ""
-    guess_list = []
-    game_over = False
+            array_of_revealed_concealed_characters.append("_")
+    view_of_revealed_concealed_characters = ""
+    for i in range(len(array_of_revealed_concealed_characters)):
+        view_of_revealed_concealed_characters += array_of_revealed_concealed_characters[i]
+    array_of_revealed_concealed_characters = []
+    game_concluded = False
     guesses_remaining = 6
 
-    hang_stage = [
+    illustration_of_hanged_stage = [
         "",
         "O",
         " O\n |",
@@ -100,108 +111,99 @@ while ON:
         "========\n||     |\n||     @\n||    /|\ \n||    / \ \n||\n||        /\n||       /\n||      /\n||     /",
         "<O__\n |\n/ \ "
         ]
-
-    text = [
-        "\nI'm sorry, but you were hanged! Don't worry though, you can be resurrected.\n",
-        "\n\n\nPROGRAM TERMINATED!!!\n\n\n",
-        "Please choose only Yes or No.\n",
-        "Would you like to play again? (yes/no): ",
-        "Your guess was incorrect. The answer was " + raw_answer + "."
-    ]
-
-    while guess != answer and guess_update != raw_answer and not game_over and guesses_remaining > 0:
-        print("So far, the clue is: " + guess_update)
-        print("Characters yet to be chosen: " + yet_to_be_chosen)
-        print("Characters already chosen: " + already_chosen + "\n")
-        guess = input("\nPlease guess a character or guess the word(s): ").lower()
-        print()
-        if len(guess) == 1:
-            if guess in answer and guess != " ":
-                if guess in yet_to_be_chosen:
-                    yet_to_be_chosen = yet_to_be_chosen.replace(guess, "")
-                    already_chosen += guess
-                    fifty_lines()
-                    print(hang_stage[6 - guesses_remaining] + "\n\n\n")
-                    print("Well done, " + guess.upper() + " is in the answer!\n")
-                    lives()
-                    guess_list = []
-                    for i in range(len(answer)):
-                        if answer[i] == guess or answer[i] in already_chosen:
-                            guess_list.append(raw_answer[i])
-                        elif answer[i] == " ":
-                            guess_list.append(" ")
+    latest_guess = ""
+    while latest_guess != lower_case_answer and view_of_revealed_concealed_characters != case_sensitive_answer and not game_concluded and guesses_remaining > 0:
+        print("So far, the clue is: " + view_of_revealed_concealed_characters)
+        print("Characters yet to be chosen: " + characters_not_used_yet)
+        print("Characters already chosen: " + characters_already_used + "\n")
+        latest_guess = input("\nPlease guess a character or guess the answer: ").lower()
+        if len(latest_guess) == 1:
+            if latest_guess in lower_case_answer and latest_guess != " ":
+                if latest_guess in characters_not_used_yet:
+                    characters_not_used_yet = characters_not_used_yet.replace(latest_guess, "")
+                    characters_already_used += latest_guess
+                    clear_screen()
+                    show_current_hang_stage()
+                    print("Well done, " + latest_guess.upper() + " is in the answer!\n")
+                    show_lives_remaining()
+                    array_of_revealed_concealed_characters = []
+                    for i in range(len(lower_case_answer)):
+                        if lower_case_answer[i] == latest_guess or lower_case_answer[i] in characters_already_used or lower_case_answer[i] in allowed_special_characters:
+                            array_of_revealed_concealed_characters.append(case_sensitive_answer[i])
+                        elif lower_case_answer[i] == " ":
+                            array_of_revealed_concealed_characters.append(" ")
                         else:
-                            guess_list.append("_")
-                    guess_update = ""
-                    for i in range(len(guess_list)):
-                        guess_update += guess_list[i]
+                            array_of_revealed_concealed_characters.append("_")
+                    view_of_revealed_concealed_characters = ""
+                    for i in range(len(array_of_revealed_concealed_characters)):
+                        view_of_revealed_concealed_characters += array_of_revealed_concealed_characters[i]
                 else:
-                    fifty_lines()
-                    print(hang_stage[6 - guesses_remaining] + "\n\n\n")
-                    print(guess + " has already been chosen. Please only choose from the")
+                    clear_screen()
+                    show_current_hang_stage()
+                    print(latest_guess + " has already been chosen. Please only choose from the")
                     print("list of characters that are yet to be chosen.\n")
-                    lives()
+                    show_lives_remaining()
             else:
-                if guess in yet_to_be_chosen:
+                if latest_guess in characters_not_used_yet:
                     if guesses_remaining > 2:
-                        yet_to_be_chosen = yet_to_be_chosen.replace(guess, "")
+                        characters_not_used_yet = characters_not_used_yet.replace(latest_guess, "")
                         not_in_answer()
-                        print("Unlucky, " + guess.upper() + " is not in the answer.")
+                        print("Unlucky, " + latest_guess.upper() + " is not in the answer.")
                         print("You have " + str(guesses_remaining) + " lives left.")
                     elif guesses_remaining < 2:
-                        yet_to_be_chosen = yet_to_be_chosen.replace(guess, "")
+                        characters_not_used_yet = characters_not_used_yet.replace(latest_guess, "")
                         not_in_answer()
-                        print("Unlucky, " + guess.upper() + " was not in the answer. The")
-                        print("answer was " + raw_answer + ". You have " + str(guesses_remaining) + " lives left.")
+                        print("Unlucky, " + latest_guess.upper() + " was not in the answer. The")
+                        print("answer was " + case_sensitive_answer + ". You have " + str(guesses_remaining) + " lives left.")
                     elif guesses_remaining == 2:
-                        yet_to_be_chosen = yet_to_be_chosen.replace(guess, "")
+                        characters_not_used_yet = characters_not_used_yet.replace(latest_guess, "")
                         not_in_answer()
-                        print("Unlucky, " + guess.upper() + " is not in the answer.")
+                        print("Unlucky, " + latest_guess.upper() + " is not in the answer.")
                         print("You have " + str(guesses_remaining) + " life left.\n")
-                elif guess in already_chosen:
-                    fifty_lines()
-                    print(hang_stage[6 - guesses_remaining] + "\n\n\n")
-                    print(guess + " has already been chosen, and it wasn't in")
+                elif latest_guess in characters_already_used:
+                    clear_screen()
+                    show_current_hang_stage()
+                    print(latest_guess + " has already been chosen, and it wasn't in")
                     print("the answer. Please only choose from the")
                     print("list of characters that are yet to be chosen.\n")
-                    lives()
+                    show_lives_remaining()
                 else:
-                    fifty_lines()
-                    print(hang_stage[6 - guesses_remaining] + "\n\n\n")
+                    clear_screen()
+                    show_current_hang_stage()
                     print("Invalid character. Please only choose from the")
                     print("list of characters that are yet to be chosen.\n")
-                    lives()
-        elif len(guess) == len(answer):
-            if guess == answer:
-                game_over = True
-            elif "  " in guess:
-                invalid()
+                    show_lives_remaining()
+        elif len(latest_guess) == len(lower_case_answer):
+            if latest_guess == lower_case_answer:
+                game_concluded = True
+            elif "  " in latest_guess:
+                two_space_bar_invalid_input()
             else:
-                wrong_guess()
-        elif len(guess) < 1:
-            fifty_lines()
-            print(hang_stage[6 - guesses_remaining] + "\n\n\n")
+                wrong_guess_ends_game()
+        elif len(latest_guess) < 1:
+            clear_screen()
+            show_current_hang_stage()
             print("You didn't choose anything, and only hit the return key. Please")
             print("choose from the list of characters that are yet to be chosen.\n")
-            lives()
+            show_lives_remaining()
         else:
-            if "  " in guess:
-                invalid()
+            if "  " in latest_guess:
+                two_space_bar_invalid_input()
             else:
-                wrong_guess()
-    if guess == answer:
-        fifty_lines()
-        print(hang_stage[7])
-        print("Well done, you guessed right! The answer is " + raw_answer + ".\n")
+                wrong_guess_ends_game()
+    if latest_guess == lower_case_answer:
+        clear_screen()
+        print(illustration_of_hanged_stage[7])
+        print("Well done, you guessed right! The answer is " + case_sensitive_answer + ".\n")
         choice = False
-        while_not_choice()
-    elif guess_update == raw_answer:
-        fifty_lines()
-        print(hang_stage[7])
-        print("Well done, you guessed all the characters right!\nThe answer is " + raw_answer + ".\n")
+        ask_user_if_they_want_to_play_again()
+    elif view_of_revealed_concealed_characters == case_sensitive_answer:
+        clear_screen()
+        print(illustration_of_hanged_stage[7])
+        print("Well done, you guessed all the characters right!\nThe answer is " + case_sensitive_answer + ".\n")
         choice = False
-        while_not_choice()
+        ask_user_if_they_want_to_play_again()
     else:
-        print(text[0])
+        print("\nI'm sorry, but you were hanged! Don't worry though, you can be resurrected.\n")
         choice = False
-        while_not_choice()
+        ask_user_if_they_want_to_play_again()
